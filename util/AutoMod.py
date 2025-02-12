a89340e3869a0404043118d295062bd5e264bd3b
leonard <leonard.vaney@epfl.ch>
2025-02-12 15:25:01 +0100

    Add a regex to check for similar character swap

--- util/AutoMod.py
+++ util/AutoMod.py
@@ -1,9 +1,38 @@
 from discord.ext import commands
 
+import re
+
+#Bidirectional correspondence
+character_connection = {
+    'i': '[i1|!]',
+    '1': '[i1|!]',
+    '|': '[i1|!]',
+    '!': '[i1|!]',
+
+    'a': '[a4]',
+    '4': '[a4]',
+    
+    'e': '[e3]',
+    '3': '[e3]',
+    
+    'o': '[o0]',
+    '0': '[o0]',
+    
+    's': '[s5]',
+    '5': '[s5]',
+    
+    't': '[t7]',
+    '7': '[t7]'
+}
+
+def create_pattern(word: str):
+    return "".join(character_connection.get(char, char) for char in word)
+
 async def check_against_automod_lists(ctx: commands.Context, message: str):
     rules = await ctx.guild.fetch_automod_rules()
     for rule in rules:
-        for string in rule.trigger.keyword_filter:
-            if string.replace("*", "") in message:
-                return False
+        regex_patterns = [create_pattern(word.replace("*", "")) for word in rule.trigger.keyword_filter]
+        combined_pattern = r"\b(" + "|".join(regex_patterns) + r")\b"
+        if re.search(combined_pattern, message, re.IGNORECASE):
+            return False
     return True
