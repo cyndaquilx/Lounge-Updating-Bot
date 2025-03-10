442b2822919ca0d4d3944936b2fa5a871295ff83
cyndaquilx <ultramk8@gmail.com>
2025-03-10 13:30:49 -0400

    fix rate limiting on reaction log

--- cogs/Reactions.py
+++ cogs/Reactions.py
@@ -1,10 +1,30 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from models import ServerConfig
 
 class Reactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.embed_queue: dict[discord.TextChannel, list[discord.Embed]] = {}
+        
+        self._embedqueue_task = self.send_queued_embeds.start()
+
+    @tasks.loop(seconds=5)
+    async def send_queued_embeds(self):
+        for channel in self.embed_queue:
+            curr_embeds: list[discord.Embed] = []
+            # remove all the embeds from the queue in backwards order
+            # (this is so that new embeds can still be added after
+            #  the function starts without them being lost)
+            # ordering doesn't really matter since this is just a log
+            for i in range(len(self.embed_queue[channel]), -1, -1):
+                e = self.embed_queue[channel].pop(i)
+                if len(curr_embeds) == 10:
+                    await channel.send(embeds=curr_embeds)
+                    curr_embeds = []
+                curr_embeds.append(e)
+            if len(curr_embeds):
+                await channel.send(embeds=curr_embeds)
 
     @commands.Cog.listener(name='on_reaction_add')
     async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
@@ -29,7 +49,9 @@ class Reactions(commands.Cog):
         else:
             reaction_str = reaction.emoji
         e.add_field(name="Emoji", value=reaction_str)
-        await channel.send(embed=e)
+        if channel not in self.embed_queue:
+            self.embed_queue[channel] = []
+        self.embed_queue[channel].append(e)
 
 async def setup(bot):
     await bot.add_cog(Reactions(bot))
