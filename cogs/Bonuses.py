0aa66dc138a5f9dc8bd9147daa348b0d14df7373
cyndaquilx <ultramk8@gmail.com>
2025-04-21 15:09:20 -0400

    add bonus csv command

--- cogs/Bonuses.py
+++ cogs/Bonuses.py
@@ -3,10 +3,11 @@ from discord import app_commands
 from discord.ext import commands
 import API.get, API.post
 from models import LeaderboardConfig
-from custom_checks import command_check_staff_roles, app_command_check_staff_roles
+from custom_checks import command_check_staff_roles, app_command_check_staff_roles, app_command_check_admin_roles
 from util import get_leaderboard, get_leaderboard_slash, update_roles
 import custom_checks
 from typing import Optional
+import csv
 
 class Bonuses(commands.Cog):
     def __init__(self, bot):
@@ -67,6 +68,33 @@ class Bonuses(commands.Cog):
         ctx = await commands.Context.from_interaction(interaction)
         lb = get_leaderboard_slash(ctx, leaderboard)
         await self.give_bonus(ctx, lb, amount, name, reason)
+
+    @app_commands.check(app_command_check_admin_roles)
+    @bonus_group.command(name="csv")
+    @app_commands.autocomplete(leaderboard=custom_checks.leaderboard_autocomplete)
+    async def bonus_csv(self, interaction: discord.Interaction, csv_file: discord.Attachment, leaderboard: Optional[str]):
+        ctx = await commands.Context.from_interaction(interaction)
+        lb = get_leaderboard_slash(ctx, leaderboard)
+        await ctx.send("Working...")
+        # takes in a CSV file with 2 columns - Name and MMR
+        file = await csv_file.read()
+        decoded_file = file.decode().splitlines()
+        row_count = len(decoded_file)
+        reader = csv.reader(decoded_file)
+        for i, row in enumerate(reader):
+            player_ids = row[:-2]
+            # tournament placement is row[-2], not used
+            bonus_amount = int(row[-1])
+            for player_id in player_ids:
+                player = await API.get.getPlayerFromLounge(lb.website_credentials, int(player_id))
+                if player is None:
+                    await ctx.send(f"Player with ID {player_id} not found")
+                    continue
+                await self.give_bonus(ctx, lb, bonus_amount, player.name, reason="Tournament Prize")
+            if (i+1) % 100 == 0:
+                await ctx.send(f"{i+1}/{row_count}")
+        await ctx.send("Done")
+
 
 async def setup(bot):
     await bot.add_cog(Bonuses(bot))
